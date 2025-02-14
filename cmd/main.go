12d3e716e5d42e4f62542d57b6c6e086ba230a1d
lptrk <patrik@Patriks-MacBook-Air.local>
2025-02-14 22:25:45 +0100

    feat(http): Establish server and auth routes

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"net/http"
 	"os"
 	"remissio-auth/internal/auth"
 
@@ -26,4 +27,24 @@ func main() {
 	if err != nil {
 		log.Fatal("Error migrating schema 'User'")
 	}
+
+	router := http.NewServeMux()
+
+	port := os.Getenv("APPLICATION_DEFAULT_PORT")
+	server := http.Server{
+		Addr:    port,
+		Handler: router,
+	}
+
+	err = server.ListenAndServe()
+	if err != nil {
+		log.Fatal("Failed to start http server")
+	}
+}
+
+func registerAuthRoutes(s *http.ServeMux) {
+	apiBasePath := os.Getenv("API_BASE_PATH")
+
+	s.HandleFunc(("POST " + apiBasePath + "/register"), func(http.ResponseWriter, *http.Request) {})
+	s.HandleFunc(("POST " + apiBasePath + "/login"), func(http.ResponseWriter, *http.Request) {})
 }
